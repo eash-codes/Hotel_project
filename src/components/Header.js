6f14a71ea0899e30338346e59babb19e53952f21
eash-codes <forwork8958@gmail.com>
2024-08-14 02:56:55 +0530

    Cleaned up app and added header footer components

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return ( 
+        <header className="header">
+            <h1> ET Hotels </h1>
+            <div className="links">
+                <a href="/">HOME</a>
+                <a href="/">ROOMS</a>
+                <a href="/">RESTAURANT</a>
+                <a href="/">SPA</a>
+                <a href="/">CONTACT</a>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
