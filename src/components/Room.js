9b5a2053fa65de393441455ed7d9fb4d6038d9d8
eash-codes <forwork8958@gmail.com>
2024-08-31 17:17:40 +0530

    added tailwind config and set room details

--- src/components/Room.js
+++ src/components/Room.js
@@ -13,13 +13,42 @@ const Room = ({room}) => {
     <div className="bg-white shadow-2xl min-h-[500px] group">
         {/*img*/}
         <div className="overflow-hidden">
-            <img className='group-hover:scale-110 transistion-all duration-300 w-full ' 
+            <img 
+            className="group-hover:scale-110 transition-all duration-300 w-full"
             src={image} 
             alt="" 
             />
         </div>
         {/*details*/}
-        <div className="bg-white shadow-lg max-w-[300px] mx-auto h-[60px]">details</div>
+        <div className='bg-white shadow-lg max-w-[300px] mx-auto h-[60px]-translate-y-1/2 flex justify-center items-center uppercase font-tertiary tracking-[1px] font-semibold text-base'>
+        <div className="flex justify-between w-[80%]">
+                {/* size */}
+                <div className="flex items-center gap-x-2">
+                    <div className="text-accent  ">
+                        <BsArrowsFullscreen className="text-[15px]" />
+                        </div>
+                        <div>
+                            <div className="flex gap-x-1">Size</div>
+                            <div>{size}m2</div>
+                        </div>
+                </div>
+                {/*  room capacity */}
+                <div className="flex items-center gap-x-2">
+                    <div className="text-accent  ">
+                        <BsPeople className="text-[18px]" />
+                        </div>
+                            <div className="flex gap-x-1">
+                            <div>Max People</div>
+                            <div>{maxPerson}</div>
+                     </div>
+                </div> 
+            </div>
+        </div>
+        {/* name */}
+        <div>
+            <Link to = {'/room/${id}'}>
+            <h3 className="h3">{ name }</h3></Link>
+        </div>
     </div> 
     );
 };
