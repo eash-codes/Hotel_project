a201c36f4085eb02ed24c845306fc267e57b66ee
eash-codes <forwork8958@gmail.com>
2024-09-04 19:13:34 +0530

    basic footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
+// components/Footer.js
+import React from 'react';
+
 const Footer = () => {
-    return (
-        <footer>
-            <p>Footer </p>
-            <hr />
-        </footer>
-    );
-}
+  return (
+    <footer className="bg-black text-white py-4 text-center">
+      <p>&copy; 2024 ET Hotels. All rights reserved.</p>
+    </footer>
+  );
+};
 
 export default Footer;
